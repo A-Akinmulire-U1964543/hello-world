6a719fcef5923735fdd7c9bcb818a9b65939947c
U1964543 AKINAYO AKINMULIRE <u1964543@unimail.hud.ac.uk>
2020-10-13 02:26:10 +0100

    Demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * A demo class.
+ *
+ * @author Akinayo Akinmulire U1964543
+ * @version September 2020
+ *
+ */
+public class HelloWorld {
+    /**
+     * A demo method.
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
